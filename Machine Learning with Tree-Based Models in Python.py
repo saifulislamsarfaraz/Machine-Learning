efe6f74d6deb29feeb5e9a5c00aba42722c07676
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-08-19 22:52:34 +0600

    okay

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -134,3 +134,18 @@ RMSE_train = (MSE(y_train, y_pred_train))**(1/2)
 
 # Print RMSE_train
 print('Train RMSE: {:.2f}'.format(RMSE_train))
+
+# Set seed for reproducibility
+SEED=1
+
+# Instantiate lr
+lr = LogisticRegression(random_state=SEED)
+
+# Instantiate knn
+knn = KNN(n_neighbors=27)
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
+
+# Define the list classifiers
+classifiers = [('Logistic Regression', lr), ('K Nearest Neighbors', knn), ('Classification Tree', dt)]
