277027aa3cbb3595dd2bcf9652d9371ff8b2e5ac
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-08-19 22:53:42 +0600

    Evaluate individual classifiers In this exercise you'll evaluate the performance of the models in the list classifiers that we defined in the previous exercise. You'll do so by fitting each classifier on the training set and evaluating its test set accuracy.
    The dataset is already loaded and preprocessed for you (numerical features are standardized) and it is split into 70% train and 30% test. The features matrices X_train and X_test, as well as the arrays of labels y_train and y_test are available in your workspace. In addition, we have loaded the list classifiers from the previous exercise, as well as the function accuracy_score() from sklearn.metrics.

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -149,3 +149,18 @@ dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
 
 # Define the list classifiers
 classifiers = [('Logistic Regression', lr), ('K Nearest Neighbors', knn), ('Classification Tree', dt)]
+
+# Iterate over the pre-defined list of classifiers
+for clf_name, clf in classifiers:
+
+    # Fit clf to the training set
+    clf.fit(X_train, y_train)
+
+    # Predict y_pred
+    y_pred = clf.predict(X_test)
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y_pred, y_test)
+
+    # Evaluate clf's accuracy on the test set
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))
