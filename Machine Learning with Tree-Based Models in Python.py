b3eb090a69ad7a993f863bbedc504b45dbfc0dc5
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-08-20 12:23:53 +0600

    OOB Score vs Test Set Score

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -221,3 +221,19 @@ bc = BaggingClassifier(base_estimator=dt,
             n_estimators=50,
             oob_score=True,
             random_state=1)
+            
+#####OOB Score vs Test Set Score
+# Fit bc to the training set
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate test set accuracy
+acc_test = accuracy_score(y_pred, y_test)
+
+# Evaluate OOB accuracy
+acc_oob = bc.oob_score_
+
+# Print acc_test and acc_oob
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))
