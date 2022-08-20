6bdd40cab276de5b40459a779b41f47545e24c32
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2022-08-20 12:16:19 +0600

    Prepare the ground

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -206,3 +206,18 @@ y_pred = bc.predict(X_test)
 # Evaluate acc_test
 acc_test = accuracy_score(y_pred, y_test)
 print('Test set accuracy of bc: {:.2f}'.format(acc_test))
+
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=8, random_state=1)
+
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt,
+            n_estimators=50,
+            oob_score=True,
+            random_state=1)
