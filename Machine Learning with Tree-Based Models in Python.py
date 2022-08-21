081a777b6703b3f07e2355cbb1adb15984b081ec
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:38:39 +0600

    Update Machine Learning with Tree-Based Models in Python.py
    Set the hyperparameter grid of RF

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -431,3 +431,36 @@ grid_dt = GridSearchCV(estimator=dt,
                        cv=5,
                        n_jobs=-1)
 
+
+
+
+
+
+
+#Evaluate the optimal tree
+# Import roc_auc_score from sklearn.metrics
+from sklearn.metrics import roc_auc_score
+
+# Extract the best estimator
+best_model = grid_dt.best_estimator_
+
+# Predict the test set probabilities of the positive class
+y_pred_proba = best_model.predict_proba(X_test)[:,1]
+
+# Compute test_roc_auc
+test_roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+# Print test_roc_auc
+print('Test set ROC AUC score: {:.3f}'.format(test_roc_auc))
+
+
+
+
+#Set the hyperparameter grid of RF
+# Define the dictionary 'params_rf'
+params_rf = {'n_estimators':[100,350,500],
+             'max_features':['log2','auto','sqrt'],
+             'min_samples_leaf':[2,10,30]}
+
+
+
