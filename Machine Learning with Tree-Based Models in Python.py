380700843be32f06d8715c547d891792ea2ee19e
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:17:23 +0600

    Update Machine Learning with Tree-Based Models in Python.py
    Set the tree's hyperparameter grid

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -404,3 +404,8 @@ rmse_test = mse_test**(1/2)
 # Print rmse_test
 print('Test set RMSE of sgbr: {:.3f}'.format(rmse_test))
 
+
+#Set the tree's hyperparameter grid
+# Define params_dt
+params_dt = {'max_depth' : [2, 3, 4],'min_samples_leaf' : [0.12, 0.14, 0.16, 0.18]}
+
