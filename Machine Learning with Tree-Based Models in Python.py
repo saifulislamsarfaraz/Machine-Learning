841a46281f146cf91a2391df0b88ab3683841461
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:09:55 +0600

    Update Machine Learning with Tree-Based Models in Python.py

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -341,3 +341,20 @@ gb.fit(X_train, y_train)
 
 # Predict test set labels
 y_pred = gb.predict(X_test)
+
+
+
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute MSE
+mse_test = MSE(y_test, y_pred)
+
+# Compute RMSE
+rmse_test = mse_test**(1/2)
+
+# Print RMSE
+print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
+
+
