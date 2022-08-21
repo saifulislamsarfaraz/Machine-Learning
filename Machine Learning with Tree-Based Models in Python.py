e60f4c0790563749bb35fb52faef562802ae70f7
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 16:53:15 +0600

    Update Machine Learning with Tree-Based Models in Python.py
    Evaluate the RF regressor

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -249,3 +249,16 @@ rf = RandomForestRegressor(n_estimators=25,
             
 # Fit rf to the training set    
 rf.fit(X_train, y_train)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Predict the test set labels
+y_pred = rf.predict(X_test)
+
+# Evaluate the test set RMSE
+rmse_test = MSE(y_test, y_pred)**(1/2)
+
+# Print rmse_test
+print('Test set RMSE of rf: {:.2f}'.format(rmse_test))
+
