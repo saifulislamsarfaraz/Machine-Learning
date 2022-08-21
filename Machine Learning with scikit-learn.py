48d5bbae906278cdee823e7083c8fccfdc65acb6
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 19:12:20 +0600

    Update Machine Learning with scikit-learn.py

--- Machine Learning with scikit-learn.py
+++ Machine Learning with scikit-learn.py
@@ -24,3 +24,26 @@ knn.fit(X,y)
 #Predict and print the label for the new data point X_new 
 new_prediction = knn.predict(X_new)
 
+
+
+
+
+#The digits recognition dataset
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits['DESCR'])
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
