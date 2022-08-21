b4edf989d77511f2dc1c9253299a622a2eeeadae
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 18:42:27 +0600

    Update Machine Learning with scikit-learn.py
    K nearest Neighbors: predict

--- Machine Learning with scikit-learn.py
+++ Machine Learning with scikit-learn.py
@@ -12,3 +12,15 @@ knn = KNeighborsClassifier(n_neighbors=6)
 
 # Fit the classifier to the data
 knn.fit(X,y)
+
+# K Nearest Neighbors: Predict 
+
+#create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+#Fit the classifier to the data 
+knn.fit(X,y)
+
+#Predict and print the label for the new data point X_new 
+new_prediction = knn.predict(X_new)
+
