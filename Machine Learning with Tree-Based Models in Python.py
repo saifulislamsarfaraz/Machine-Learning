874692d394f871756ec3d47e86d7843dcbf29bb6
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:14:31 +0600

    Update Machine Learning with Tree-Based Models in Python.py

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -361,3 +361,22 @@ rmse_test = mse_test**(1/2)
 print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
 
 
+
+
+
+
+
+Regression with SGB
+
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate sgbr
+sgbr = GradientBoostingRegressor(max_depth=4,
+                                subsample=0.9,
+                                max_features=0.75,
+                                n_estimators=200,
+                                random_state=2)
+
+
+
