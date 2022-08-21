914fd22ab30d1f9b7006591d0fd979e4dd9df9fb
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:06:21 +0600

    Update Machine Learning with Tree-Based Models in Python.py

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -277,3 +277,56 @@ plt.show()
 
 
 
+Define the AdaBoost classifier
+
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(max_depth=2, random_state=1)
+
+# Instantiate ada
+ada = AdaBoostClassifier(base_estimator=dt, n_estimators=180, random_state=1)
+
+
+
+
+
+
+Train the AdaBoost classifier
+# Fit ada to the training set
+ada.fit(X_train, y_train)
+
+# Compute the probabilities of obtaining the positive class
+y_pred_proba = ada.predict_proba(X_test)[:,1]
+
+
+
+
+
+Evaluate the AdaBoost classifier
+
+# Import roc_auc_score
+from sklearn.metrics import roc_auc_score
+
+# Evaluate test-set roc_auc_score
+ada_roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+# Print roc_auc_score
+print('ROC AUC score: {:.2f}'.format(ada_roc_auc))
+
+
+
+
+
+Define the GB regressor
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate gb
+gb = GradientBoostingRegressor(max_depth=4,
+            n_estimators=200,
+            random_state=2)
