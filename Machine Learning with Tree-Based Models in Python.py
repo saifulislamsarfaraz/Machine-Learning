56d7c658d5a12cb215a841f4b5d506671ffe8115
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:19:05 +0600

    Update Machine Learning with Tree-Based Models in Python.py
    Search for the optimal tree

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -409,3 +409,25 @@ print('Test set RMSE of sgbr: {:.3f}'.format(rmse_test))
 # Define params_dt
 params_dt = {'max_depth' : [2, 3, 4],'min_samples_leaf' : [0.12, 0.14, 0.16, 0.18]}
 
+
+
+
+
+
+
+
+
+
+
+
+#Search for the optimal tree
+# Import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+# Instantiate grid_dt
+grid_dt = GridSearchCV(estimator=dt,
+                       param_grid=params_dt,
+                       scoring='roc_auc',
+                       cv=5,
+                       n_jobs=-1)
+
