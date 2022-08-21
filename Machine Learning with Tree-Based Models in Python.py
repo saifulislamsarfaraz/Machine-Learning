0bad3f5d0a6b288c2f2cc69afe3047b300d3e7e0
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-08-21 17:43:48 +0600

    Update Machine Learning with Tree-Based Models in Python.py
    Search for the optimal forest

--- Machine Learning with Tree-Based Models in Python.py
+++ Machine Learning with Tree-Based Models in Python.py
@@ -463,4 +463,24 @@ params_rf = {'n_estimators':[100,350,500],
              'min_samples_leaf':[2,10,30]}
 
 
+#Search for the optimal forest
+# Import GridSearchCV
+from sklearn.model_selection import  GridSearchCV
+
+# Instantiate grid_rf
+grid_rf = GridSearchCV(estimator=rf,
+                       param_grid=params_rf,
+                       scoring='neg_mean_squared_error',
+                       cv=3,
+                       verbose=1,
+                       n_jobs=-1)
+
+
+
+
+
+
+
+
+
 
